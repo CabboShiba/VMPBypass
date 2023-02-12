11b145eb313308eed9fbe5507449bcda9eb4597c
Cabbo <92642446+CabboShiba@users.noreply.github.com>
2023-02-12 15:26:36 +0100

    Add files via upload

---
+++ VMPBypass/Bypass/MarshalHook.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMPBypass.Bypass
+{
+    internal class MarshalHook
+    {
+        [HarmonyPatch(typeof(System.Runtime.InteropServices.Marshal), nameof(System.Runtime.InteropServices.Marshal.AllocHGlobal), new[] { typeof(int) })]
+        class FixEnvironmentExit
+        {
+            [STAThread]
+            static bool Prefix(int cb)
+            {
+                try
+                {
+                    cb--;
+                }
+                catch (Exception ex)
+                {
+                    Program.Log($"Error during AntiDBG Bypass:\n{ex.Message}", "ERROR", ConsoleColor.Red);
+                    cb = 1;
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ VMPBypass/Program.cs
@@ -0,0 +1,60 @@
+﻿using HarmonyLib;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+namespace VMPBypass
+{
+    internal class Program
+    {
+        private static string AssemblyLoad = "";
+        static void Main(string[] args)
+        {
+            try
+            {
+                AssemblyLoad = args[0];
+            }
+            catch(Exception ex)
+            {
+                while (!File.Exists(AssemblyLoad) || new FileInfo(AssemblyLoad).Extension != ".exe")
+                {
+                    Console.Clear();
+                    Log("Please provide a valid executable File [.EXE]: ", "CONFIG", ConsoleColor.Cyan);
+                    AssemblyLoad = Console.ReadLine();
+                }
+                Console.Clear();
+            }
+            try
+            {
+                object[] parameters = null;
+                var assembly = Assembly.LoadFile(Path.GetFullPath(AssemblyLoad));
+                var paraminfo = assembly.EntryPoint.GetParameters();
+                parameters = new object[paraminfo.Length];
+                Harmony patch = new Harmony("VMPRotectAntiDebuggerBypass_https://github.com/CabboShiba");
+                patch.PatchAll(Assembly.GetExecutingAssembly());
+                assembly.EntryPoint.Invoke(null, parameters);
+            }
+            catch (Exception ex)
+            {
+                Log($"Could not load {AssemblyLoad}\n{ex}", "ERROR", ConsoleColor.Red);
+            }
+            
+            Console.ReadLine();
+        }
+
+        private static bool WinForm = false;
+        public static void Log(string Data, string Type, ConsoleColor Color)
+        {
+            if (WinForm == true)
+            {
+                MessageBox.Show(Data, Type, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                Console.ForegroundColor = Color;
+                Console.WriteLine($"[{DateTime.Now.ToString("HH:mm:ss")} - {Type}] {Data}");
+                Console.ResetColor();
+            }
+        }
+    }
+}
